b1ab6be2004921aceac873a34cb03057b7495306
Subrata Kundu <subrata.kundu30@gmail.com>
2019-05-08 15:16:06 +0530

    Currency Exchange Service and Currency Conversion Service

---
+++ currency-exchange-service/src/main/java/com/education/microservices/currencyexchangeservice/ExchangeValueRepository.java
@@ -0,0 +1,9 @@
+package com.education.microservices.currencyexchangeservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long>{
+
+	ExchangeValue findByFromAndTo(String from, String to);
+	
+}
---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+Insert into exchange_value(id, currency_from, currency_to, conversion_Multiple, port) 
+values(1000L, 'USD', 'INR', 65, 0);
+Insert into exchange_value(id, currency_from, currency_to, conversion_Multiple, port) 
+values(1001L, 'EUR', 'INR', 75, 0);
+Insert into exchange_value(id, currency_from, currency_to, conversion_Multiple, port) 
+values(1002L, 'AUD', 'INR', 55, 0);
+Insert into exchange_value(id, currency_from, currency_to, conversion_Multiple, port) 
+values(1003L, 'POUND', 'INR', 90, 0);
