56fa373214708ada76f2ca57239ff1adde91119a
Subrata Kundu <subrata.kundu30@gmail.com>
2019-05-09 02:07:45 +0530

    Adding Seulth Dependencies and Sampler Code

--- currency-conversion-service/src/main/java/com/education/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/education/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -7,10 +7,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 //@FeignClient(name="currency-exchange-service" , url="localhost:8000") --> If more than one port is there
 
-@FeignClient(name="currency-exchange-service")
+//@FeignClient(name="currency-exchange-service") //Commented to call the servive via Zuul Api gateway 
+
+@FeignClient(name="netflix-zuul-api-gateway-server")
 @RibbonClient(name="currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
-	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	//@GetMapping("/currency-exchange/from/{from}/to/{to}") // Commented as now service call is via Zuul gateway.
+	@GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
 	public CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
 }
